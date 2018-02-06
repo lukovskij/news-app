9c2e5ea8bbfa4cd993fa97e2de06c0b7aa74c281
alexander.kravchuk <alexander.kravchuk@nakitel.com>
2018-02-06 19:00:35 +0200

    add calndar

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 // main components
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
+import DatePicker from './DatePicker'
 
 export default class App extends Component {
 
@@ -11,6 +12,9 @@ export default class App extends Component {
         return(
             <div>
                 <UserForm articles= { this.props.articles }/>
+                <div style={{margin:"20px 0"}}>
+                    <DatePicker/>
+                </div>
                 <ArticleList articles={ this.props.articles }/>
             </div>
         )
